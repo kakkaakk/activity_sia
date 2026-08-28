9df7980ce348d99bf897a25f3bdd2de634bb270c
kakkaakk <kakaoowo777@gmail.com>
2026-08-28 09:53:20 +0800

    feat: server setup

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const app = require('express')
 
 //Server Setup : Express module will be handling the function name
 const server = app()  //hold crtl then click app to check
+//const PORT = 6767
+//const HOSTNAME = '0.0.0.0'
 server.listen(6767, '0.0.0.0', () => {
     console.log('Server is running')
 })  //hold crtl then click listen to check
