b5e7dff5ce5d928d16372d9de4470b9153084e62
kakkaakk <kakaoowo777@gmail.com>
2026-08-28 09:31:03 +0800

    feat: server setup

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// test this sesmantic commit
+
+
+//Package imports : Because we installed express package 
+const app = require('express') 
+
+//Server Setup : Express module will be handling the function name
+const server = app()  //hold crtl then click app to check
+server.listen(6767, '192.168.2.47', () => {
+    console.log('Server is running')
+})  //hold crtl then click listen to check
